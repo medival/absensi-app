090c3637076d0e19772f148532c378dd5ade58c3
John Doe <dev.adipurnomo@gmail.com>
2024-04-25 02:08:24 +0700

    chore: try use https

--- config/app.php
+++ config/app.php
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://absensi-app-7vxnwnalqq-et.a.run.app'),
 
     'asset_url' => env('ASSET_URL'),
 
--- docker/startup.sh
+++ docker/startup.sh
@@ -3,4 +3,5 @@ php-fpm -D \
     && while ! socat -dd UNIX-CONNECT:/run/php-fpm.sock -; do sleep 0.1; done;
 
 sed -i "s,LISTEN_PORT,${PORT:-80},g" /etc/nginx/http.d/default.conf \
+    && sed -i "s,APP_URL,${APP_URL},g" /etc/nginx/http.d/default.conf \
     && nginx -g 'daemon off;'
