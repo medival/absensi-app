7515530ca1fecc379d78f0240b9a95647fd7a9ba
John Doe <dev.adipurnomo@gmail.com>
2024-04-26 03:15:35 +0700

    chore: test

--- public/js/auth/login.js
+++ public/js/auth/login.js
@@ -10,35 +10,44 @@ loginForm.addEventListener("submit", async function (e) {
     const formData = new FormData(this);
 
     setLoadingButton(loginFormButton, true);
-    const { res, data } = await fetchWithToken(this.action, {
-        method: "POST",
-        body: formData,
-    });
-    setLoadingButton(loginFormButton, false);
+    try {
+        const { res, data } = await fetchWithToken(this.action, {
+            method: "POST",
+            body: formData,
+        });
+        setLoadingButton(loginFormButton, false);
 
-    if (res.status == 422) {
-        const errorElement = builtValidationElement(data.errors);
+        if (res.status === 422) {
+            const errorElement = builtValidationElement(data.errors);
+            toast.show({
+                title: "Error",
+                body: errorElement,
+                colorClass: toast.TOAST_DANGER,
+            });
+        } else if (res.status === 400) {
+            toast.show({
+                title: "Informasi",
+                body: `<span>${data.message}</span>`,
+                colorClass: toast.TOAST_DANGER,
+            });
+        } else if (res.status === 200) {
+            toast.show({
+                title: "Informasi",
+                body: `<span>${data.message}</span>`,
+                colorClass: toast.TOAST_SUCCESS,
+            });
+            window.location.assign(data.redirect_to);
+        } else {
+            throw new Error("Unexpected response from server");
+        }
+    } catch (error) {
+        console.error("An error occurred:", error);
+        setLoadingButton(loginFormButton, false);
+        // Handle other types of errors, e.g., network errors
         toast.show({
             title: "Error",
-            body: errorElement,
+            body: "An error occurred while processing your request. Please try again later.",
             colorClass: toast.TOAST_DANGER,
         });
-    }
-
-    if (res.status == 400) {
-        toast.show({
-            title: "Informasi",
-            body: `<span>${data.message}</span>`,
-            colorClass: toast.TOAST_DANGER,
-        });
-    }
-
-    if (res.status == 200) {
-        toast.show({
-            title: "Informasi",
-            body: `<span>${data.message}</span>`,
-            colorClass: toast.TOAST_SUCCESS,
-        });
-        window.location.assign(data.redirect_to);
     }
 });
