ab729c308559e63b30ed23a7db4a6ec1d695017b
John Doe <dev.adipurnomo@gmail.com>
2024-04-26 05:13:08 +0700

    chore: more verbose

--- public/js/utils.js
+++ public/js/utils.js
@@ -2,6 +2,7 @@ export const fetchWithToken = (url, options = {}) => {
     return new Promise(async (resolve, reject) => {
         let res, data;
         try {
+            // Membuat permintaan fetch dengan menggabungkan opsi dan menambahkan header CSRF
             res = await fetch(url, {
                 ...options,
                 headers: {
@@ -11,13 +12,29 @@ export const fetchWithToken = (url, options = {}) => {
                         .getAttribute("content"),
                 },
             });
+
+            // Memeriksa apakah respons memiliki status sukses (status 200-299)
+            if (!res.ok) {
+                // Jika respons tidak berhasil, kirim respons ke catch
+                throw new Error(`Server responded with error: ${res.status} ${res.statusText}`);
+            }
+
+            // Parsing data JSON dari respons
             data = await res.json();
+            // Menyelesaikan Promise dengan respons dan data yang diperoleh
             resolve({ res, data });
         } catch (error) {
-            reject({ res, data, error });
+            // Menangani kesalahan
+            reject({
+                message: "Error occurred while fetching data:",
+                url,
+                options,
+                error: error.message // Menyertakan pesan kesalahan yang terperinci
+            });
         }
     });
 };
+
 
 export const builtValidationElement = (errors) => {
     const keyOfErrors = Object.getOwnPropertyNames(errors);
